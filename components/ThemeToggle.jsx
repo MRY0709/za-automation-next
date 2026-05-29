15a2928aadd373f0e6740e470954f3f8a838bd32
Ume <umemaryam2004@gmail.com>
2026-05-29 10:08:12 +0500

    Fixed theme localStorage issue

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,19 +1,25 @@
 "use client";
+
 import { useEffect, useState } from "react";
 
 function ThemeToggle() {
-  const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem("theme") !== "light";
-  });
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "light") {
+      document.body.classList.add("light-theme");
+      setDarkMode(false);
+    }
+  }, []);
 
   useEffect(() => {
     if (darkMode) {
       document.body.classList.remove("light-theme");
-
       localStorage.setItem("theme", "dark");
     } else {
       document.body.classList.add("light-theme");
-
       localStorage.setItem("theme", "light");
     }
   }, [darkMode]);
